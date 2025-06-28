81c7b69bf2ee4e7dc8949fc090d8f8950185badb
janehcarl <janehcarl@gmail.com>
2025-06-28 16:50:20 +0300

    .

--- talkdrove.js
+++ talkdrove.js
@@ -10,7 +10,7 @@
     "env": {
       "SESSION_ID": {
         "description": "Put your SESSION_ID here. Make sure it starts with ey~  or get session id from here,  https://charle-ke.onrender.com/",
-        "value":"",
+        "value":"eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoid09BdjhGL3FYelFBRmFRczI3UVR0Wk5hckh2RFZDcDlsUzRvNHpOQWhWUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRnNVQ2w4alkvcHpycCszbWVqYitjSzNwVUIyYUE0dnFDaVF2T3lyeWl6OD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIwTUptZDlzK2pUTTJuTFZta3BHQ3lGaElSaTB6eEg0K1FqRmlMY3JTR1VBPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJFUFhRV1RwSThRK25aNHlzOGM5dUlIWG5nYkZoMUUwVVRRVkRKZGR2VkU4PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjZPZGNoZjI5Vk04RUdZWXV2NnJzTk1HTzUzeHA1WkVOOVowMG5TN3l3bWM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlAwaEJmdXBTL1BibGRna2c3Y1hWRlVBcTUxSFpIZ2dweDNKRzFqZzlUQlk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0xJRHhIcSs5SlQveGpRSHVLTFZZTnoyWm1SUm1TOFkvNkhlS0l5VEtuRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiWWZPSVZWRi8xNWM1VS9QV29SUm8rVUxTWUIrMU9JRjk4WGxweUxwTmlDWT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNrU1JuRDd0eU1kL093UXRHalJoQ2Zpc1JEYUJTUW5aY1ZSYVZqVnRrRy9vRURWQ0xnaDZRUjJiVXp0cm9tWG01SXJwU3FLOHdBNURVWUdyL1EybGhRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTUsImFkdlNlY3JldEtleSI6IlNXemhycVdzNVVaMFNhejlrUVZtYktlZzlVV1Z3YTZNeDFGaHhta0diODg9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbeyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiMTU2MzcwQTEwOTkxNkFFNDNCOERGRjRCNkVGRTI0NDcifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODAwMX0seyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiOTJBOEJGOTk2Mjc3OTkzRUVGOTcwOTdFOUQwODQzN0MifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODAwMX0seyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiM0Q5NzgyQTNEODM1ODY4QzAyMDIyRjdGMjlDRTRGRDMifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODAxN30seyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiRkVDQjY4RUY5NzVCNkIzOTc5QzQ1NTVCRkY3NkNCOTYifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODAyN30seyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiMDcwNTA0NzlBODAzNjk1NDY1MEY1NzdDOTcyQTU2ODYifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODAzNn0seyJrZXkiOnsicmVtb3RlSmlkIjoiOTY4OTMzODc5NTNAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiREFFMkVDRjBEMTc0MTU2MTc5MDk1RDYyRUNCRUQ5NTIifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MTExODA0NX1dLCJuZXh0UHJlS2V5SWQiOjMyLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzIsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiNk00QlFFQTEiLCJtZSI6eyJpZCI6Ijk2ODkzMzg3OTUzOjdAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxNDcxMTE0Mzc5NDcwNDM6N0BsaWQiLCJuYW1lIjoiVGFzaHnwn6WwIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNPT1VvdjBFRUtEaC84SUdHQUlnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI1czVXMnhBOFp5MlF6bUZVUktXdjg1YkUrdHhhcVU0ZXlFVlRKVUdjbkdvPSIsImFjY291bnRTaWduYXR1cmUiOiJjL3VjQWl5MEJMZ1Z1OFRwS0V5RlNOV2tqWjFvZVB1WWRQZSttbW1YNkZUM0ZMR1k3Q25PUEdsRmZJZlRNVzNBcXZpbXR0UW1salZZUXhHQ3FyRHJEdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiNEY4Q0JyQlAwYjc4bE1Fd1NPekZGMGZpN2lpeVJVSFBFbm5GU2M1RzRhODcvb2dvZktFZXFrYnJDcWpHS054dFl3K1JHbVZiYWZrS1BkSFM5SWlnakE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5Njg5MzM4Nzk1Mzo3QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmViT1Z0c1FQR2N0a001aFZFU2xyL09XeFByY1dxbE9Ic2hGVXlWQm5KeHEifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBSUlFZz09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1MTExNzk5NywibGFzdFByb3BIYXNoIjoiUFdrNUIiLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUpJdCJ9",
         "required": true        
     },
       "PREFIX": {
@@ -30,8 +30,8 @@
     },        
     "ALWAYS_ONLINE": {
       "description": "Make it true if want always online.",
-      "required": false,
-      "value": "false"  
+      "required": true,
+      "value": "true"  
    },      
     "AUTO_VOICE": {
       "description": "Make it true if want automatic voice reply .",
@@ -50,7 +50,7 @@
    }, 
     "AUTO_STATUS_REPLY": {
       "description": "Make it true for auto reply msg on status seen.",
-      "required": true,
+      "required": false,
       "value": "false"
    }, 
     "AUTO_STATUS_MSG": {
@@ -66,7 +66,7 @@
     "OWNER_NUMBER": {
       "description": "put the owner number for bot.",
       "required": false,
-      "value": "255763834140"
+      "value": "96893387953"
    }, 
     "BOT_NAME": {
       "description": "Type here the bot name.",
@@ -90,27 +90,27 @@
     },    
     "AUTO_RECORDING": {
       "description": "Make it true if you want auto recoding.",
-      "required": false,
-      "value": "false"
+      "required": true,
+      "value": "true"
     },
     "AUTO_TYPING": {
       "description": "Make it true if you want auto typing.",
-      "required": false,
-      "value": "false"
+      "required": true,
+      "value": "true"
     },
     "AUTO_REACT": {
       "description": "Make it true if you want react on every message.",
-      "required": false,
-      "value": "false"
+      "required": true,
+      "value": "true"
     },    
     "CUSTOM_REACT": {
       "description": "Make it true if you want custom reactions.",
-      "required": false,
-      "value": "false"      
+      "required": true,
+      "value": "true"      
     },    
     "CUSTOM_REACT_EMOJIS": {
       "description": "put here custom react react emojis.",
-      "required": false,
+      "required": true,
       "value": "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍"    
     }, 
     "READ_MESSAGE": {
